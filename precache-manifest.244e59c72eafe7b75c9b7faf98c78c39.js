bcde216cb997b5341936b51957081f9c9ab6a637
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-08-28 19:24:37 +0200

    Updates

---
+++ precache-manifest.244e59c72eafe7b75c9b7faf98c78c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce408d027f02c454d7d60bbc9930938",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "84b4be83f835eb8c15d9",
+    "url": "/crwn-clothing/static/css/main.900f5003.chunk.css"
+  },
+  {
+    "revision": "44ba21966c11c203ce04",
+    "url": "/crwn-clothing/static/js/2.af7c4b43.chunk.js"
+  },
+  {
+    "revision": "84b4be83f835eb8c15d9",
+    "url": "/crwn-clothing/static/js/main.e82ee716.chunk.js"
+  },
+  {
+    "revision": "cc4489cf2189afec394d",
+    "url": "/crwn-clothing/static/js/runtime~main.dfb7735a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8744bffba9f42d16e292b6c2409e0045.js"
+  "/crwn-clothing/precache-manifest.244e59c72eafe7b75c9b7faf98c78c39.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crwn-clothing/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
