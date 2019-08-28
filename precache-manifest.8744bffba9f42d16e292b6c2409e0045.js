bf068a95753f41325c61adda01530314c5b4077c
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-08-28 19:22:00 +0200

    Updates

---
+++ precache-manifest.8744bffba9f42d16e292b6c2409e0045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5203e71be63d4649d24cd70c23b2ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "969390ae41123b6008cf",
+    "url": "/static/css/main.900f5003.chunk.css"
+  },
+  {
+    "revision": "8ed54f9ae37b86010410",
+    "url": "/static/js/2.f685fbda.chunk.js"
+  },
+  {
+    "revision": "969390ae41123b6008cf",
+    "url": "/static/js/main.e732820a.chunk.js"
+  },
+  {
+    "revision": "48d41c13aacf878df560",
+    "url": "/static/js/runtime~main.b0422439.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  }
+]);
