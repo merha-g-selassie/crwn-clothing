307cdf157a4864b1f9c6eb4ae478384870829d89
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-08-29 02:22:46 +0200

    added .env file for firebase config variables

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const config = {
-  apiKey: 'AIzaSyByHLL0O8YL0GazUxmUrTYleUWCEuOCy5s',
-  authDomain: 'crwn-db-d71a2.firebaseapp.com',
-  databaseURL: 'https://crwn-db-d71a2.firebaseio.com',
-  projectId: 'crwn-db-d71a2',
-  storageBucket: '',
-  messagingSenderId: '508236332374',
-  appId: '1:508236332374:web:fae8b7b3ccb8ac69'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
