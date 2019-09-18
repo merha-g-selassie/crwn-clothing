07324d29bda1c15819b9d16d82e80bc7836c6456
Merhawi Ghebre Selassie <merhawi.gs@gmail.com>
2019-09-18 22:08:26 +0200

    commit

--- src/components/collection-preview/CollectionPreview.jsx
+++ src/components/collection-preview/CollectionPreview.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import CollectionItem from '../collection-item/CollectionItem';
 import './CollectionPreview.scss';
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
-        {items
-          .filter((item, idx) => idx < 4)
-          .map(item => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, idx) => idx < 4)
+        .map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-  );
-};
+  </div>
+);
 
 export default CollectionPreview;
--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import CollectionItem from '../../components/collection-item/CollectionItem';
 import { selectCollection } from '../../redux/shop/shop.selectors';
 import './Collection.scss';
-const Collection = ({ collection, match }) => {
+const Collection = ({ collection }) => {
   const { title, items } = collection;
   return (
     <div className='collection-page'>
